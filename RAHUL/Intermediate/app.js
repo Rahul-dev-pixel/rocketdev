939c98da8bff25d60b0e8cb33124b7dbc304ce0c
Rahul-dev-pixel <m.rahul361@gmail.com>
2023-08-10 18:02:00 +0530

    updated program

--- RAHUL/Intermediate/app.js
+++ RAHUL/Intermediate/app.js
@@ -14,3 +14,40 @@ console.log(chalk.yellowBright('Enter Valid Credentials'));
 console.log(chalk.hex('#83139C').bold('Bold gray!'));
 
 login(true);
+
+const votercheck= (age)=>
+{
+if(age>=18)
+{
+    console.log(chalk.greenBright("valid voter"));
+}
+else
+{
+    console.log(chalk.redBright("Invalid voter"));;
+}
+}
+//calling voter check method
+votercheck(18);
+votercheck(16);
+
+// checking for prime or not
+const checkPrime = (digit) =>
+{
+    let counter = 0;
+    for(i=1;i<=digit;i++)
+    {
+        if(digit % i == 0)
+        {
+            counter++;
+        }
+    }
+    if(counter==2)
+    {
+        console.log(chalk.greenBright("the given number"+ digit + "is prime"));
+    }
+    else
+    {
+        console.log(chalk.redBright("the given number" + digit + "is not prime"));
+    }
+}
+checkPrime(7);//calling checkPrime by giving value
