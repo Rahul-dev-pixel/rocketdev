80913be7bc99838ce8fe8ed2797aa12f1c4a51df
MEHER CHAITANYA <47631184+meherchaitanyabandaru@users.noreply.github.com>
2023-08-10 08:27:10 +0530

    Update app.js

--- RAHUL/Intermediate/app.js
+++ RAHUL/Intermediate/app.js
@@ -1,4 +1,16 @@
 // import chalk from 'chalk'; 
 const chalk=require('chalk');
 
-console.log(chalk.redBright('Hello world!'));
+
+const login=(isUserLoggedIn)=>{
+    if(isUserLoggedIn){
+        console.log(chalk.bgGreenBright('Congratulations welcome to Dashboard')); 
+    }else{
+        console.log(chalk.bgRedBright('Login Failed. Please Enter Valid Credentials'));
+    }
+};
+// 13909C
+console.log(chalk.yellowBright('Enter Valid Credentials'));
+console.log(chalk.hex('#83139C').bold('Bold gray!'));
+
+login(true);
